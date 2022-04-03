6c5a529364876cf4276039113716ec07f43f6380
AnnaSemichaeva <tdhnss@gmail.com>
2022-04-03 23:14:35 +0300

    added some important elements

--- ZombieConga/GameViewController.swift
+++ ZombieConga/GameViewController.swift
@@ -15,6 +15,14 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         let scene = GameScene(size: CGSize(width: 2048, height: 1536))
         let skView = self.view as! SKView
-        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 }
