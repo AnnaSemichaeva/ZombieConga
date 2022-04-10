37e0edb1f6b54edd438c52c43e39cebdba067dee
AnnaSemichaeva <tdhnss@gmail.com>
2022-04-10 23:45:20 +0300

    homework

---
+++ MyUtils.swift
@@ -0,0 +1,13 @@
+//
+//  MyUtils.swift
+//  ZombieConga
+//
+//  Created by macuser on 4/10/22.
+//
+
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
