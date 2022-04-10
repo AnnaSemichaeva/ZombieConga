851e72ffe6b7a131d4433a5ada476e33aa109aca
AnnaSemichaeva <tdhnss@gmail.com>
2022-04-10 23:47:06 +0300

    Update MyUtils.swift

--- MyUtils.swift
+++ MyUtils.swift
@@ -11,3 +11,7 @@ import CoreGraphics
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
+
+func += (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
